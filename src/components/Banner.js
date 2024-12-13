e1c207fed186710cbbde1baba08474a3d8fe44f2
raysmith745 <ray@bolser.co.uk>
2024-12-13 15:09:34 +0000

    Use vite framework

--- src/components/Banner.js
+++
@@ -1,9 +0,0 @@
-export default function Banner() {
-	return (
-		<div className="h-32 w-full border-2 
-						h-screen flex items-center 
-						justify-center bg-emerald-500 text-white">
-		<p className="text-2xl"> Welcome to GeeksforGeeks Blog Page!!!</p>
-		</div>
-	)
-}
